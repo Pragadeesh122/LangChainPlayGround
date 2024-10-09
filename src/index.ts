86fa45e3858d69ad50c06aa68cfb53f39095a2e4
Pragadeesh122 <pragan189@gmail.com>
2024-10-09 14:43:42 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {ChatOpenAI} from "@langchain/openai";
+import {ChatPromptTemplate} from "@langchain/core/prompts";
+import {StringOutputParser} from "@langchain/core/output_parsers";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const model = new ChatOpenAI({
+  model: "gpt-4",
+});
+const parser = new StringOutputParser();
+const systemTemplate = "Translate the following into {language}:";
+
+const promptTemplate = ChatPromptTemplate.fromMessages([
+  ["system", systemTemplate],
+  ["user", "{text}"],
+]);
+
+const llmChain = promptTemplate.pipe(model).pipe(parser);
+
+(async () => {
+  const result = await llmChain.invoke({language: "italian", text: "hi"});
+  console.log(result);
+})();
